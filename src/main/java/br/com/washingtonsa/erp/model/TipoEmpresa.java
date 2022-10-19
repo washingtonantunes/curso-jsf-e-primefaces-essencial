2f18b57f2405795c7de12a763918a2345d619438
Washington Antunes <washington.cead@gmail.com>
2022-10-19 17:10:40 -0300

    Mapeando as entidades

---
+++ src/main/java/br/com/washingtonsa/erp/model/TipoEmpresa.java
@@ -0,0 +1,19 @@
+package br.com.washingtonsa.erp.model;
+
+public enum TipoEmpresa {
+
+	MEI("Microempreendedor Individual"), 
+	EIRELI("Empresa Individual de Responsabilidade Limitada"),
+	LTDA("Sociedade Limitada"),
+	SA("Sociedade Anônima");
+	
+	private String descricao;
+
+	TipoEmpresa(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+}
