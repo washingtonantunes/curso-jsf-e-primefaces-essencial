ffec973f32bf4221d338183359b45f189ec06903
Washington Antunes <washington.cead@gmail.com>
2022-10-22 14:57:57 -0300

    Criando o gerenciador de transações com CDI

---
+++ src/main/java/br/com/washingtonsa/erp/service/CadastroEmpresaService.java
@@ -0,0 +1,26 @@
+package br.com.washingtonsa.erp.service;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+
+import br.com.washingtonsa.erp.model.Empresa;
+import br.com.washingtonsa.erp.repository.Empresas;
+import br.com.washingtonsa.erp.util.Transacional;
+
+public class CadastroEmpresaService implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Empresas empresas;
+	
+	@Transacional
+	public void salvar(Empresa empresa) {
+		empresas.guardar(empresa);
+	}
+	
+	public void excluir(Empresa empresa) {
+		empresas.remover(empresa);
+	}
+}
