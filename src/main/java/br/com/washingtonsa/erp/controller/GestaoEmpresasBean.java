e8a37ca8789b763e40a2bc82e2884e7b137560e0
Washington Antunes <washington.cead@gmail.com>
2022-10-29 00:19:12 -0300

    Criando um conversor customizado

--- src/main/java/br/com/washingtonsa/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/br/com/washingtonsa/erp/controller/GestaoEmpresasBean.java
@@ -3,13 +3,16 @@ package br.com.washingtonsa.erp.controller;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.convert.Converter;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.washingtonsa.erp.model.Empresa;
+import br.com.washingtonsa.erp.model.RamoAtividade;
 import br.com.washingtonsa.erp.model.TipoEmpresa;
 import br.com.washingtonsa.erp.repository.Empresas;
+import br.com.washingtonsa.erp.repository.RamoAtividades;
 import br.com.washingtonsa.erp.util.FacesMessages;
 
 @Named
@@ -24,9 +27,14 @@ public class GestaoEmpresasBean implements Serializable {
 	@Inject
 	private FacesMessages messages;
 
+	@Inject
+	private RamoAtividades ramoAtividades;
+
 	private List<Empresa> listaEmpresas;
 
 	private String termoPesquisa;
+
+	private Converter ramoAtividadeConverter;
 
 	public void pesquisar() {
 		listaEmpresas = empresas.pesquisar(termoPesquisa);
@@ -40,6 +48,14 @@ public class GestaoEmpresasBean implements Serializable {
 		listaEmpresas = empresas.todas();
 	}
 
+	public List<RamoAtividade> completarRamoAtividade(String termo) {
+		List<RamoAtividade> listaRamoAtividades = ramoAtividades.pesquisar(termo);
+
+		ramoAtividadeConverter = new RamoAtividadeConverter(listaRamoAtividades);
+
+		return listaRamoAtividades;
+	}
+
 	public List<Empresa> getListaEmpresas() {
 		return listaEmpresas;
 	}
@@ -51,8 +67,12 @@ public class GestaoEmpresasBean implements Serializable {
 	public void setTermoPesquisa(String termoPesquisa) {
 		this.termoPesquisa = termoPesquisa;
 	}
-	
+
 	public TipoEmpresa[] getTiposEmpresa() {
 		return TipoEmpresa.values();
+	}
+
+	public Converter getRamoAtividadeConverter() {
+		return ramoAtividadeConverter;
 	}
 }
