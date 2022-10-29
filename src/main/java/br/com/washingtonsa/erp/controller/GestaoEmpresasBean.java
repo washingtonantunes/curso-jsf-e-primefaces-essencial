98a02edbb4e7f1f950b3da58f22fc3d3d9920b52
Washington Antunes <washington.cead@gmail.com>
2022-10-29 18:25:08 -0300

    Selecionando linhas da tabela de dados

--- src/main/java/br/com/washingtonsa/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/br/com/washingtonsa/erp/controller/GestaoEmpresasBean.java
@@ -110,4 +110,12 @@ public class GestaoEmpresasBean implements Serializable {
 	public Empresa getEmpresa() {
 		return empresa;
 	}
+	
+	public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+	
+	public boolean isEmpresaSelecionada() {
+		return empresa != null && empresa.getId() != null;
+	}
 }
