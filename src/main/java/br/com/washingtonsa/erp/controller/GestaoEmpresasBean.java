efad4addd289010128cb85c6971a92c6899945ae
Washington Antunes <washington.cead@gmail.com>
2022-10-28 23:17:10 -0300

    Implementando a pesquisa

--- src/main/java/br/com/washingtonsa/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/br/com/washingtonsa/erp/controller/GestaoEmpresasBean.java
@@ -9,25 +9,45 @@ import javax.inject.Named;
 
 import br.com.washingtonsa.erp.model.Empresa;
 import br.com.washingtonsa.erp.repository.Empresas;
+import br.com.washingtonsa.erp.util.FacesMessages;
 
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private Empresas empresas;
-	
+
+	@Inject
+	private FacesMessages messages;
+
 	private List<Empresa> listaEmpresas;
-	
+
+	private String termoPesquisa;
+
+	public void pesquisar() {
+		listaEmpresas = empresas.pesquisar(termoPesquisa);
+
+		if (listaEmpresas.isEmpty()) {
+			messages.info("Sua consulta não retornou registros.");
+		}
+	}
+
 	public void todasEmpresas() {
 		listaEmpresas = empresas.todas();
 	}
-	
+
 	public List<Empresa> getListaEmpresas() {
 		return listaEmpresas;
 	}
-	
-	
+
+	public String getTermoPesquisa() {
+		return termoPesquisa;
+	}
+
+	public void setTermoPesquisa(String termoPesquisa) {
+		this.termoPesquisa = termoPesquisa;
+	}
 }
--- src/main/java/br/com/washingtonsa/erp/repository/Empresas.java
+++ src/main/java/br/com/washingtonsa/erp/repository/Empresas.java
@@ -12,39 +12,39 @@ import br.com.washingtonsa.erp.model.Empresa;
 public class Empresas implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private EntityManager manager;
 
 	public Empresas() {
-		
+
 	}
-	
+
 	public Empresas(EntityManager manager) {
 		this.manager = manager;
 	}
-	
+
 	public Empresa porId(Long id) {
 		return manager.find(Empresa.class, id);
 	}
-	
+
 	public List<Empresa> pesquisar(String nome) {
-		String jpql = "from Empresa where nomeFantasia like :nomeFantasia";
-		
+		String jpql = "from Empresa where razaoSocial like :razaoSocial";
+
 		TypedQuery<Empresa> query = manager.createQuery(jpql, Empresa.class);
-		query.setParameter("nomeFantasia", nome + "%");
-		
+		query.setParameter("razaoSocial", nome + "%");
+
 		return query.getResultList();
 	}
-	
+
 	public List<Empresa> todas() {
 		return manager.createQuery("from Empresa", Empresa.class).getResultList();
 	}
-	
+
 	public Empresa guardar(Empresa empresa) {
 		return manager.merge(empresa);
 	}
-	
+
 	public void remover(Empresa empresa) {
 		empresa = porId(empresa.getId());
 		manager.remove(empresa);
